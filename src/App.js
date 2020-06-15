64ce90f2db35e7b8403a1b842d43f124e59b2534
ellie <ellie.tms1371@gmail.com>
2020-06-15 19:30:55 +0100

    add router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav';
+import About from './about';
+import Shop from './shop';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 
 function App() {
   return (
+    <Router> 
     <div className="App">
-     salam
+      <Nav />
+      <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/shop" component={Shop} />
+      </Switch>
     </div>
+    </Router>
   );
 }
+
+
+const Home = () => {
+  return(
+  <div>
+    <h1>
+      Home
+    </h1>
+  </div>
+  )
+}
+
 
 export default App;
---
+++ src/Nav.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./App.css"
+
+function Nav() {
+  return (
+    <div>
+        <nav>
+            <h3>
+                Logo
+            </h3>
+            <ul className="nav-links">
+                <li>
+                About
+                </li>
+                <li>
+                Shop
+                </li>
+            </ul>
+        </nav>
+    </div>
+  );
+}
+
+export default Nav;
