0f2d4563331659329f0b0da4e30130973bd6efa9
ellie <ellie.tms1371@gmail.com>
2020-06-15 19:35:24 +0100

    about shop links

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import "./App.css"
+import "./App.css";
+import {link, Link} from 'react-router-dom';
 
 function Nav() {
+
+  const navStyle = {
+    color: 'white'
+  }
+
   return (
     <div>
         <nav>
@@ -9,12 +15,16 @@ function Nav() {
                 Logo
             </h3>
             <ul className="nav-links">
+              <Link to="/about" style={navStyle}>
                 <li>
                 About
                 </li>
+                </Link>
+                <Link to="/shop" style={navStyle}>
                 <li>
                 Shop
                 </li>
+                </Link>
             </ul>
         </nav>
     </div>
