844bd7105e60c84dd2e8f055c84484bd4d4d940a
ellie <ellie.tms1371@gmail.com>
2020-06-15 19:47:20 +0100

    shop fetch

--- src/shop.js
+++ src/shop.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 
 function Shop() {
+
+  useEffect(() => {
+    fetchItems();
+  },[])
+
+
+  const fetchItems = async () => {
+    const data = await fetch("https://fortnite-public-api.theapinetwork.com/prod09/upcoming/get")
+    console.log(data)
+  }
+
+
   return (
     <div>
         <h1>
